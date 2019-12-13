12ca40bb96e18771d2732d40758970044c7b76ab
Dheimison Siqueira <dheimisonlo@gmail.com>
2019-12-12 23:19:09 -0300

    added new description in readme, and added new click functions

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # Diary Control
 ## Esta é uma ferramenta em desenvolvimento, que basicamente consiste em fazer um controle das atividades diárias.
 
-- Em breve novas funções e um novo design será adicionado.
+### Tecnologias Usadas:
+- HTML5
+- CSS3
+- JavaScript
+
+### Como usar :
+- Para testar, nenhuma instalação adicional será necessária.
+- Apenas abra o Index.HTML
+
+### Em breve novas funções e um novo design será adicionado.
+
+### Homepage :
 ![Homepage](./img/Homepage.png)
--- js/modules/handleClick.js
+++ js/modules/handleClick.js
@@ -1,32 +1,64 @@
 export default function handleClick() {
   const daysBtn = document.querySelectorAll('.days li');
   const closeBtn = document.querySelectorAll('.close');
+  const menuBtn = document.querySelector('.btnMenu');
+  const menuMobile = document.querySelector('.menuMobile');
+  const menuBack = document.querySelector('.backIcon');
+  const calendarMobile = document.querySelectorAll('.calendarMobile');
+  const sectionToday = document.querySelector('.today');
+  const sectionCalendar = document.querySelector('.calendar');
   const modalDay = document.querySelector('.modal-day-container');
   const modalMonth = document.querySelector('.modal-month');
-  const historyMenu = document.querySelector('.menu .hist')
+  const historyMenu = document.querySelector('.menu .hist');
   const eventArray = ['click', 'touchstart'];
   
-  function addActive(modal) {
-    modal.classList.add('active');
+  function addClass(element, classes) {
+    element.classList.add(classes);
   }
   
-  function removeActive() {
-    modalDay.classList.remove('active');
-    modalMonth.classList.remove('active');
+  function removeClass(element, classes) {
+    element.classList.remove(classes);
   }
   
   
   eventArray.forEach(evento => {
+
     daysBtn.forEach(item => {
       item.addEventListener(evento, () => {
-        addActive(modalDay);
+        addClass(modalDay, 'active');
       });
-    })
+    });
+
     historyMenu.addEventListener(evento, () => {
-      addActive(modalMonth);
-    })
-    closeBtn.forEach((btn => {
-      btn.addEventListener(evento, removeActive);
-    }))
+      addClass(modalMonth, 'active');
+    });
+
+    menuBtn.addEventListener(evento, () => {
+      addClass(menuMobile, 'activeMobile')
+    });
+
+    menuBack.addEventListener(evento, () => {
+      removeClass(menuMobile, 'activeMobile')
+    });
+    calendarMobile[0].addEventListener(evento, () => {
+        addClass(sectionToday, 'todayOff');
+        addClass(calendarMobile[0], 'backBtn');
+        removeClass(sectionCalendar, 'calendarOff');
+        removeClass(calendarMobile[1], 'backBtn');
+    });
+    calendarMobile[1].addEventListener(evento, () => {
+      removeClass(sectionToday, 'todayOff');
+      addClass(calendarMobile[1], 'backBtn');
+      addClass(sectionCalendar, 'calendarOff');
+      removeClass(calendarMobile[0], 'backBtn');
+  })
+
+    closeBtn[0].addEventListener(evento, () => {
+      removeClass(modalDay, 'active')
+    });
+
+    closeBtn[1].addEventListener(evento, () => {
+      removeClass(modalMonth, 'active')
+    });
   })
 }
